8dc5ea79358b92a4f3bea2b4693140a023154937
Ali Yahya <amyahya@gmail.com>
2018-07-04 10:33:13 -0400

    Chef 14 updates.
    Signed-off-by: Ali Yahya <amyahya@gmail.com>

--- resources/gemset.rb
+++ resources/gemset.rb
@@ -4,6 +4,8 @@ property :user, [String], default: 'root'
 property :ruby_string, [String], required: true, name_property: true
 
 action :create do
+  user = new_resource.user
+  ruby_string = new_resource.ruby_string
   if rvm.gemset?(ruby_string)
     Chef::Log.info("Gemset #{user} #{ruby_string} already exist. Nothing to update.")
   else
@@ -16,6 +18,8 @@ end
 
 %w[delete update pristine prune].each do |action_name|
   action action_name.to_sym do
+    user = new_resource.user
+    ruby_string = new_resource.ruby_string
     if rvm.gemset?(ruby_string)
       Chef::Log.info("#{action_name.capitalize} gemset #{ruby_string} for user #{user}.")
       rvm.send("gemset_#{action_name}".to_sym, ruby_string)
--- resources/ruby.rb
+++ resources/ruby.rb
@@ -7,6 +7,10 @@ property :patch, [String, NilClass], default: nil
 property :default, [TrueClass, FalseClass, NilClass], default: nil
 
 action :install do
+  user = new_resource.user
+  version = new_resource.version
+  patch = new_resource.patch
+  default = new_resource.default
   if rvm.ruby?(version)
     Chef::Log.debug "Ruby #{version} already installed for user #{user}"
   else
@@ -14,16 +18,18 @@ action :install do
     Chef::Log.debug "Install ruby #{version} for user #{user}"
     rvm.ruby_install(version, patch)
     rvm.gemset_create(version)
-    updated_by_last_action(true)
   end
   rvm.ruby_set_default(version) if default
 end
 
 %i[remove uninstall reinstall].each do |action_name|
   action action_name do
+    user = new_resource.user
+    version = new_resource.version
+    patch = new_resource.patch
+    default = new_resource.default
     if rvm.ruby?(version)
       Chef::Log.debug "#{action_name.to_s.capitalize} ruby #{version} for user #{user}"
-      updated_by_last_action(true)
     else
       Chef::Log.debug "Ruby #{version} is not installed for user #{user}"
     end
