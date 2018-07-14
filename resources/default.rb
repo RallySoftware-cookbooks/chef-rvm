5387f9673a00055006931b60eeab73c7bbccd7c5
Ali Yahya <amyahya@gmail.com>
2018-07-13 22:39:20 -0400

    Fix syntax. (#4)
    Signed-off-by: Ali Yahya <amyahya@gmail.com>

--- resources/default.rb
+++ resources/default.rb
@@ -9,16 +9,18 @@ property :rubies, [Hash, Array, String], default: {}
 property :rvmrc, [Hash, NilClass], default: nil
 
 action :install do
+  user = new_resource.user
+  rubies_arr = new_resource.rubies
   unless rvm.rvm?
     rvm.rvm_install
+    # new_resource.updated_by_last_action(true)
   end
-  rubies_arr = new_resource.rubies
   if rubies_arr
     rubies_arr = Array(rubies_arr) if rubies_arr.is_a?(String)
     rubies_arr.each do |ruby_string, options|
       options ||= {}
-      chef_rvm_ruby "#{new_resource.user}:#{ruby_string}" do
-        user new_resource.user
+      chef_rvm_ruby "#{user}:#{ruby_string}" do
+        user user
         version ruby_string
         patch options['patch']
         default options['default']
@@ -29,22 +31,24 @@ action :install do
 end
 
 action :upgrade do
+  user = new_resource.user
   if rvm.rvm?
-    Chef::Log.info "Upgrade RVM for user #{new_resource.user}"
+    Chef::Log.info "Upgrade RVM for user #{user}"
     rvm.rvm_get(:stable)
-    new_resource.updated_by_last_action(true)
+    # new_resource.updated_by_last_action(true)
   else
-    Chef::Log.info "Rvm is not installed for #{new_resource.user}"
+    Chef::Log.info "Rvm is not installed for #{user}"
   end
 end
 
 action :implode do
+  user = new_resource.user
   if rvm.rvm?
-    Chef::Log.info "Implode RVM for user #{new_resource.user}"
+    Chef::Log.info "Implode RVM for user #{user}"
     rvm.rvm_implode
-    new_resource.updated_by_last_action(true)
+    # new_resource.updated_by_last_action(true)
   else
-    Chef::Log.info "Rvm is not installed for #{new_resource.user}"
+    Chef::Log.info "Rvm is not installed for #{user}"
   end
 end
 
@@ -52,6 +56,7 @@ action_class.class_eval do
   include ChefRvmCookbook::RvmResourceHelper
 
   def create_or_update_rvmvc
+    user = new_resource.user
     if rvm.system?
       rvmrc_file = '/etc/rvmrc'
       rvm_path = '/usr/local/rvm/'
@@ -63,7 +68,7 @@ action_class.class_eval do
     template rvmrc_file do
       cookbook 'chef_rvm'
       source 'rvmrc.erb'
-      owner new_resource.user
+      owner user
       mode '0644'
       variables(
         system_install: rvm.system?,
