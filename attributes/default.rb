b6006471f88a193b8741fd47b767f439c2b6d290
Zach Malone <william.malone@broadcom.com>
2019-10-14 11:43:19 -0600

    We don't need sqllite or subversion for this

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
 default['chef_rvm']['packages'] = value_for_platform(
   ['centos', 'redhat', 'fedora'] => {
-    default: %w[bash tar gzip git subversion sqlite-devel]
+    default: %w[bash tar gzip git]
   },
   'ubuntu' => {
-    default: %w[bash tar gzip git subversion]
+    default: %w[bash tar gzip git]
   },
-  'default' => %w[bash tar gzip git subversion]
+  'default' => %w[bash tar gzip git]
 )
 
 default['chef_rvm']['keyserver'] = 'hkp://keyserver.ubuntu.com'
